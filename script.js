482d5efe0a90701f4814334f39b5ccec1bc0ef21
Alejandro Rodrigeuz <alejandro.xsc@gmail.com>
2019-03-19 20:36:09 -0700

    made the create event function neater and work better so we can pass parameters into it eventually

--- script.js
+++ script.js
@@ -3,17 +3,38 @@ const modalBtn = document.getElementById('modal-btn');
 const closeBtn = document.querySelector('.close');
 const createEvent = document.getElementById('create');
 
-function createElements () {
-  var newEl = document.createElement('div');
-  var elHeader = document.createElement('h1');
-  elHeader.innerText = "Hello";
-  newEl.appendChild(elHeader);
-  newEl.classList.add('jones');
-  var newELHeader =document.createElement('div');
-  newELHeader.classList.add('media-body');
+function showModal(){
+  document.getElementById('eventModal').setAttribute('aria-hidden', 'false');
+}
 
-  document.getElementById('mainEvent').appendChild(newEl);
-  console.log('div created');
+
+function createElements () {
+
+  //Creating New ELements
+  var newElContainer = document.createElement('div');
+  var newElBody = document.createElement('div');
+  var newElHeader = document.createElement('h5');
+  var newElText = document.createElement('p');
+
+
+
+  //Assign all the bootstrap classes to the ELements
+
+  newElContainer.classList.add('media');
+  newElBody.classList.add('media-body');
+
+
+
+  //Append Elements Together
+
+  document.getElementById('mainEvent').appendChild(newElContainer);
+  newElContainer.appendChild(newElBody);
+  newElBody.appendChild(newElHeader);
+  newElBody.appendChild(newElText);
+
+  newElHeader.innerText = 'Event Title';
+  newElText.innerText = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+  console.log('created new div');
 }
 
 // Events
